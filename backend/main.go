2ed78bb5b7337506fafe01ad7a1a4a48abf24424
David Gordon <davidgordondev@gmail.com>
2024-09-14 16:35:41 -0400

    Add CORS middleware

--- backend/main.go
+++ backend/main.go
@@ -7,6 +7,17 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "http://localhost:4200")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		c.Next()
+	}
+}
+
 func main() {
 	ctx := data.CreateConn()
 	defer ctx.Close()
@@ -14,6 +25,7 @@ func main() {
 	orders := orders.GetAllOrders(ctx)
 
 	r := gin.Default()
+	r.Use(CORSMiddleware())
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"orders": orders,
